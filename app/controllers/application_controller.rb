d03b3dfddd950293f281110fb4eda02ed63d3728
rasdinho <ras.dinho7@gmail.com>
2020-06-09 01:08:56 -0400

    soccer-app-backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+    def secret_key #this our key noy anyone can code a token
+        "hello" # could be anything
+    end
+
+
+    def encode(payload)
+        JWT.encode(payload, secret_key, 'HS256')
+      end
+      #given a token "abc.123" => payload {user_id: ?}
+      def decode(token)
+        JWT.decode(token, secret_key, true, {algorithm: "HS256"})[0]
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :coaches
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'auth#create'
+  get '/decode_token', to: 'users#profile'
 end
